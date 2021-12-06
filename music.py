c2ebe9b3eadc6718b43fe44c054e8780d68b2f7b
Suleiman Mai-Abba <68306565+Sulayman-ma@users.noreply.github.com>
2021-12-06 14:10:29 +0100

    Update music.py

--- music.py
+++ music.py
@@ -21,22 +21,29 @@ parser = argparse.ArgumentParser(description="A command line tool used to easily
 parser.add_argument('-n', type=int, help='number of songs to copy, 30 by default', default=30)
 args = vars(parser.parse_args())
 
-# music library path
-music = Path.home() / 'Music'
 
 library = []
 selection = []
 
+# music library path
+music = Path.home() / 'Music'
+
 for root, folders, songs in os.walk(music):
     for song in songs:
-        if song.endswith('.mkv') or song.endswith('.mp3'):
+        if song.endswith('mkv') or song.endswith('mp3'):
             library.append(Path(os.path.join(root, song)))
+
+# create destination folder
+try:
+    Path.mkdir(Path.home() / 'Desktop\\Random Selection')
+except FileExistsError:
+    pass
 
 total = 0
 for i in range(args['n']):
-    song = choice(library)
+    song = Path(choice(library))
+    shutil.copy(song, str(Path.home() / 'Desktop\\Random Selection'))
     total += song.stat().st_size / 1048576
-    shutil.copy(song, str(Path.home() / "Desktop" / "Random Selection"))
     library.remove(song)
 
-print('Total size = {:.2f}MB'.format(total))
+print('Files copied successfully ✔\nTotal size = {:.2f}MB'.format(total))
